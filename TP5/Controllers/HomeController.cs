d21fde1206a8f60fad44bbdc734195020e5e178f
Agustin <agus_gymnas2009@hotmail.com>
2020-11-11 13:32:58 -0300

    Pirmer commit: lento pero contento

---
+++ TP5/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TP5.Entidades;
+using TP5.Models;
+
+namespace TP5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        List<Tareas> tareas = new List<Tareas>();
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult MostrarTareas()
+        {
+            return View();
+        }
+
+        public IActionResult AltaTarea()
+        {
+            return View(new Tareas());
+        }
+
+        [HttpPost]
+        public IActionResult CrearTarea(string _descripcion, int _duracion)
+        {
+            Tareas tarea = new Tareas();
+            tarea.Descripcion = _descripcion;
+            tarea.Duracion = _duracion;
+
+            tareas.Add(tarea);
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TP5/Entidades/Tareas.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TP5.Entidades
+{
+    public class Tareas
+    {
+        private int id;
+        private string descripcion;
+        private int duracion;
+        private string estado;
+
+        public int Id { get => id; set => id = value; }
+
+        [Required(ErrorMessage = "Tiene que definir una descripcion")]
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        
+        [Required(ErrorMessage = "Tiene que definir una duracion")]
+        public int Duracion { get => duracion; set => duracion = value; }
+
+        [Required(ErrorMessage = "Tiene que definir un estado de la tarea")]
+        public string Estado { get => estado; set => estado = value; }
+
+        public Tareas()
+        {
+            Id = 0;
+            Descripcion = "";
+            Duracion = 0;
+            Estado = "";
+        }
+
+        public Tareas(int _id, string _descripcion, int _duracion, string _estado)
+        {
+            Id = _id;
+            Descripcion = _descripcion;
+            Duracion = _duracion;
+            Estado = _estado;
+        }
+    }
+}
