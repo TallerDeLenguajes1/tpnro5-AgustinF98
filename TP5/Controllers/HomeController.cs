137e549bf4fdca9f6c1e1da70fe3fd0f28246b9a
Agustin <agus_gymnas2009@hotmail.com>
2020-11-13 14:34:41 -0300

    TP terminado con lagrimas de estudiante estresado (?

--- TP5/Controllers/HomeController.cs
+++ TP5/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -13,7 +15,7 @@ namespace TP5.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        List<Tareas> tareas = new List<Tareas>();
+        private static List<Tareas> listaTareas = new List<Tareas>();
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -31,7 +33,29 @@ namespace TP5.Controllers
 
         public IActionResult MostrarTareas()
         {
-            return View(tareas);
+            listaTareas.Clear();
+
+            string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), @"BDatos\BDtp5.db");
+            var conection = new SQLiteConnection(cadena);
+            conection.Open();
+
+            var command = conection.CreateCommand();
+            command.CommandText = "SELECT * FROM Tareas";
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Tareas tarea = new Tareas();
+                tarea.Id = Convert.ToInt32(reader["IDtarea"]);
+                tarea.Descripcion = Convert.ToString(reader["Descripcion"]);
+                tarea.Duracion = Convert.ToInt32(reader["Duracion"]);
+                tarea.Estado = Convert.ToString(reader["Estado"]);
+                listaTareas.Add(tarea);
+            }
+
+            conection.Close();
+
+            return View(listaTareas);
         }
 
         public IActionResult AltaTarea()
@@ -40,15 +64,65 @@ namespace TP5.Controllers
         }
 
         [HttpPost]
-        public IActionResult CrearTarea(string _descripcion, int _duracion)
+        public IActionResult CrearTarea(string Descripcion, int Duracion, string Estado)
         {
-            Tareas tarea = new Tareas();
-            tarea.Descripcion = _descripcion;
-            tarea.Duracion = _duracion;
+            string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), @"BDatos\BDtp5.db");
+            var conection = new SQLiteConnection(cadena);
+            conection.Open();
 
-            tareas.Add(tarea);
+            var command = conection.CreateCommand();
+            command.CommandText = "INSERT INTO Tareas(Descripcion, Duracion, Estado) VALUES (@Descripcion, @Duracion, @Estado)";
+            command.Parameters.AddWithValue("@Descripcion", Descripcion);
+            command.Parameters.AddWithValue("@Duracion", Duracion);
+            command.Parameters.AddWithValue("@Estado", Estado);
+            command.ExecuteNonQuery();
 
-            return View();
+            conection.Close();
+
+            return Redirect("/Home/MostrarTareas");
+        }
+
+        public IActionResult BajaTarea(int ID)
+        {
+            string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), @"BDatos\BDtp5.db");
+            var conection = new SQLiteConnection(cadena);
+            conection.Open();
+
+            var command = conection.CreateCommand();
+            command.CommandText = "DELETE FROM Tareas WHERE IDtarea = @ID";
+            command.Parameters.AddWithValue("@ID", ID);
+            command.ExecuteNonQuery();
+
+            conection.Close();
+
+            return Redirect("/Home/MostrarTareas");
+        }
+
+        public IActionResult Modificacion(int ID)
+        {
+            Tareas tareaAux = new Tareas();
+            tareaAux = Helper.BuscarPorID(ID, listaTareas);
+
+            return View(tareaAux);
+        }
+
+        public IActionResult ModificacionTarea(int ID, string Descripcion, int Duracion, string Estado)
+        {
+            string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), @"BDatos\BDtp5.db");
+            var conection = new SQLiteConnection(cadena);
+            conection.Open();
+
+            var command = conection.CreateCommand();
+            command.CommandText = "UPDATE Tareas SET Descripcion = @Descripcion, Duracion = @Duracion, Estado = @Estado WHERE IDtarea = @ID";
+            command.Parameters.AddWithValue("@ID", ID);
+            command.Parameters.AddWithValue("@Descripcion", Descripcion);
+            command.Parameters.AddWithValue("@Duracion", Duracion);
+            command.Parameters.AddWithValue("@Estado", Estado);
+            command.ExecuteNonQuery();
+            
+            conection.Close();
+
+            return Redirect("/Home/MostrarTareas");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ TP5/Entidades/Helper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TP5.Entidades
+{
+    public class Helper
+    {
+        public static Tareas BuscarPorID(int _id, List<Tareas> ListaTareas)
+        {
+            foreach(var Tarea in ListaTareas)
+            {
+                if (Tarea.Id == _id)
+                {
+                    return Tarea;
+                }
+            }
+            return null;
+        }
+    }
+}
